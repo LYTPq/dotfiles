b6c13045bed2d831e676ebd305c70d9b96bde86d
Rahman <raxman20080@gmail.com>
2026-05-30 01:15:28 +0200

    minor

--- rofi/rofi/launcher/launcher.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-dir="$HOME/.config/rofi/launcher"
-theme='main'
-
-# Run
-rofi \
-  -show drun \
-  -theme ${dir}/${theme}.rasi
--- yazi/yazi/init.lua
+++
@@ -1,5 +0,0 @@
-require("no-status"):setup()
-
-require("smart-enter"):setup({
-	open_multi = true,
-})
